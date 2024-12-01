47edfb6d589f1e840e9656fed555740085a1a1a0
Anas Abuelhaag <anaselhaag@gmail.com>
2024-12-02 01:49:54 +0200

    Remove next-sitemap configuration and generated sitemap; clean up package.json

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://ucode.live',
-    generateRobotsTxt: true, // (optional) Generate a robots.txt file
-    // ...other options
-};
