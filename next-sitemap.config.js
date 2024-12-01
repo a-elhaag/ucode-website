13e0890cf5020d49bc694c8d34a12cc1a2dc814c
Anas Abuelhaag <anaselhaag@gmail.com>
2024-12-02 01:41:36 +0200

    Add next-sitemap integration and configuration; update package.json and package-lock.json

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ucode.live',
+    generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    // ...other options
+};
