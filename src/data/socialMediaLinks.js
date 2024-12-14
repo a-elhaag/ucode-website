a8777b54f019f3695e6833ca8afd7963bc02308a
Anas Abuelhaag <anaselhaag@gmail.com>
2024-12-14 21:53:56 +0200

    Update Instagram link in social media data

--- src/data/socialMediaLinks.js
+++ src/data/socialMediaLinks.js
@@ -1,6 +1,6 @@
 const socialMediaLinks = {
     facebook: "https://www.facebook.com/ucode.live",
-    instagram: "https://www.instagram.com/ucode.live/",
+    instagram: "https://www.instagram.com/ucode.eg/",
     whatsapp: "https://wa.me/message/E2CET4ZAD3IHB1",
     linkedin: "https://www.linkedin.com/company/u-code-eg/",
     medium: null, // No link, icon will be hidden
