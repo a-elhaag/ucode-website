e8dba23141e9c0d6c0a58f7f8b982bf58cc581fc
Anas Abuelhaag <anaselhaag@gmail.com>
2024-12-21 19:39:27 +0200

    Simplify SignIn component usage by removing unnecessary props

--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => {
     return (
         <div className="flex items-center justify-center h-screen bg-gray-100">
-            <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+            <SignIn />
         </div>
     );
 };
