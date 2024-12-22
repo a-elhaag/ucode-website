64ab9e9a3c80492e8d59728e3b4d635467668f98
Anas Abuelhaag <anaselhaag@gmail.com>
2024-12-22 15:03:12 +0200

    Update Hero component styles and correct social media links

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -13,14 +13,14 @@ const Hero = () => {
             <div className="absolute inset-0 bg-gray bg-opacity-50 flex flex-col items-center justify-center text-center">
                 {/* Hero Text with Animation */}
                 <motion.h1
-                    className="text-4xl font-bold text-white"
+                    className="text-4xl font-bold text-black"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8 }}
                 >
                     U
                     <motion.span
-                        className="text-yellow-500"
+                        className="text-blue-500"
                         initial={{ opacity: 0, scale: 0.8 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ delay: 0.5, duration: 0.5 }}
@@ -29,7 +29,7 @@ const Hero = () => {
                     </motion.span>
                     Code
                     <motion.span
-                        className="text-yellow-500"
+                        className="text-blue-500"
                         initial={{ opacity: 0, scale: 0.8 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ delay: 1, duration: 0.5 }}
@@ -41,7 +41,7 @@ const Hero = () => {
 
                 {/* Subheading */}
                 <motion.p
-                    className="mt-4 text-lg text-gray-200"
+                    className="mt-4 text-lg text-black-200"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 1.5, duration: 0.8 }}
--- src/data/socialMediaLinks.js
+++ src/data/socialMediaLinks.js
@@ -1,8 +1,8 @@
 const socialMediaLinks = {
-    facebook: "https://www.facebook.com/ucode.live",
-    instagram: "https://www.instagram.com/ucode.eg/",
-    whatsapp: "https://wa.me/message/E2CET4ZAD3IHB1",
-    linkedin: "https://www.linkedin.com/company/u-code-eg/",
+    facebook: "https://www.facebook.com/ucodjke.live",
+    instagram: "https://www.instagram.com/ubjbcode.eg/",
+    whatsapp: "https://wa.me/message/E2CET4ZAD3IHnmnB1",
+    linkedin: "https://www.linkedin.com/company/u-codejkbk/",
     medium: null, // No link, icon will be hidden
     youtube: null,
     tiktok: null, // No link, icon will be hidden
